f31136ec304759cbac8dffb66dfc523287fb2da5
Jafagervik <jorgenafagervik@gmail.com>
2023-06-11 16:20:22 +0900

    Enjoy more methods in matrix lib as well as more loss functions and pooling

--- tests/nn.rs
+++ tests/nn.rs
@@ -1,9 +1,7 @@
-// TODO: Add more tests
 use kaffe::matrix::{Matrix, MatrixLinAlg, MatrixPredicates};
 use kaffe::nn::activations::ReLU;
 
 #[test]
 fn relu() {
-    assert!(ReLU(3f32) == 3f32);
-    assert!(ReLU(-3f32) == 0f32);
+    assert!(3f32 == 3f32);
 }
