20e963dc2098f9bea3588f1c802112ca0615b5bf
Jafagervik <jorgenafagervik@gmail.com>
2023-06-11 17:07:26 +0900

    Fix test module

--- tests/nn.rs
+++ tests/nn.rs
@@ -1,7 +1,8 @@
-use kaffe::matrix::{Matrix, MatrixLinAlg, MatrixPredicates};
-use kaffe::nn::activations::ReLU;
+use kaffe::matrix::Matrix;
+use kaffe::nn::activation::ReLU;
 
 #[test]
 fn relu() {
-    assert!(3f32 == 3f32);
+    let mat = Matrix::new(vec![-2.0, -4.0, 6.0], (3, 1));
+    assert_eq!(ReLU(&mat).data, vec![0.0, 0.0, 6.0]);
 }
